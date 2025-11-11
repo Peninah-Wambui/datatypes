295fd1fe53d42e9be8bf6a4191ab2f8fab18084f
Peninah <165781054+Peninah-Wambui@users.noreply.github.com>
2025-11-11 08:42:32 -0800

    Add files via upload

---
+++ datatypes.js
@@ -0,0 +1,23 @@
+//intergers
+//float
+//doubles
+//logical
+
+//const //the value of the varible will remain constant 
+
+//let // the value of the varibale will/can change
+
+console.log("SIMPLE CALCULATOR:")
+const a=300
+const b=234
+
+let sum=a+b
+let multiple=a*b
+let division=a/b
+
+let sub=a-b
+
+console.log("The sum is ",+sum)
+console.log("The mulplication:",+multiple)
+console.log("The division is :",division)
+console.log("the sub is ",+sub)
